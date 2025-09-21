7ee8ab3d8b34921d8fd20633dc25a6877394899b
Potsawee <potsaweee.p@gmail.com>
2025-09-21 13:00:41 +0700

    chore: format go code

--- internal/handlers/quiz/handler.go
+++ internal/handlers/quiz/handler.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Quiz struct {
-	A string `json:"a"`
-	B string `json:"b"`
+	A      string `json:"a"`
+	B      string `json:"b"`
 	Option string `json:"option"`
 }
 
 type CheckRequest struct {
-	A string `json:"a"` 
-	B string `json:"b"` 
-	Answer string `json:"answer"` 
+	A      string `json:"a"`
+	B      string `json:"b"`
+	Answer string `json:"answer"`
 	Option string `json:"option"`
 }
 
@@ -24,11 +24,11 @@ type CheckResponse struct {
 }
 
 func GetQuiz(c *fiber.Ctx) error {
-	mode := c.Query("mode","random")
-	options := []string{"add","sub","xor","shift"}
-	quiz,err := genQuiz(mode,&options) 
+	mode := c.Query("mode", "random")
+	options := []string{"add", "sub", "xor", "shift"}
+	quiz, err := genQuiz(mode, &options)
 	if err != nil {
-		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error":"invalid mode"})	
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "invalid mode"})
 	}
 	return c.JSON(quiz)
 }
@@ -39,18 +39,18 @@ func CheckQuiz(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid input"})
 	}
 
-	a,err := strconv.ParseUint(req.A,2,8)
+	a, err := strconv.ParseUint(req.A, 2, 8)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid input"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid input"})
 	}
-	b,err := strconv.ParseUint(req.B,2,8)
+	b, err := strconv.ParseUint(req.B, 2, 8)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"invalid input"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid input"})
 	}
 
-	res,err := checkQuiz(req.Option,a,b,req.Answer) 
+	res, err := checkQuiz(req.Option, a, b, req.Answer)
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":err.Error()})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	return c.JSON(res)
--- internal/handlers/quiz/service.go
+++ internal/handlers/quiz/service.go
@@ -7,46 +7,46 @@ import (
 	"slices"
 )
 
-func stringInSlice(a string,list *[]string) bool {
+func stringInSlice(a string, list *[]string) bool {
 	return slices.Contains(*list, a)
 }
 
-func genQuiz(mode string,options *[]string) (*Quiz,error){
+func genQuiz(mode string, options *[]string) (*Quiz, error) {
 	var option string
 	randombyte1 := rand.Intn(256)
 	randombyte2 := rand.Intn(256)
-	A := fmt.Sprintf("%08b",randombyte1)
-	B := fmt.Sprintf("%08b",randombyte2)
+	A := fmt.Sprintf("%08b", randombyte1)
+	B := fmt.Sprintf("%08b", randombyte2)
 	if mode == "random" {
 		idx := rand.Intn(len(*options))
 		option = (*options)[idx]
-	} else if(stringInSlice(mode,options)) {
+	} else if stringInSlice(mode, options) {
 		option = mode
 	} else {
 		return nil, errors.New("invalid mode")
 	}
-	quiz := Quiz{A: A, B: B,Option:option}
-	return &quiz,nil
+	quiz := Quiz{A: A, B: B, Option: option}
+	return &quiz, nil
 }
 
-func checkQuiz(option string,a uint64,b uint64,answer string)(*CheckResponse,error) {
-	var hexStr string 
+func checkQuiz(option string, a uint64, b uint64, answer string) (*CheckResponse, error) {
+	var hexStr string
 	switch option {
 	case "add":
 		sum := (a + b) & 0xFF
-		hexStr = fmt.Sprintf("%02x",sum)
+		hexStr = fmt.Sprintf("%02x", sum)
 	case "sub":
 		result := (a - b) & 0xFF
-		hexStr = fmt.Sprintf("%02x",result)
+		hexStr = fmt.Sprintf("%02x", result)
 	case "xor":
 		result := (a ^ b)
-		hexStr = fmt.Sprintf("%02x",result)
+		hexStr = fmt.Sprintf("%02x", result)
 	case "shift":
 		result := (a << 1) & 0xFF
-		hexStr = fmt.Sprintf("%02x",result)
+		hexStr = fmt.Sprintf("%02x", result)
 	default:
-		return nil,errors.New("invalid input option")
+		return nil, errors.New("invalid input option")
 	}
-	return &CheckResponse{IsCorrect: hexStr == answer},nil
+	return &CheckResponse{IsCorrect: hexStr == answer}, nil
 
 }
